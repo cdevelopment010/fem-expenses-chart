4b9d8f3d9f4d96ae43e9314e54c1e9c3394ffef9
Craig <cdevelopment010@gmail.com>
2024-01-10 21:01:54 +0000

    Feat: desktop version

--- index.js
+++ index.js
@@ -7,28 +7,35 @@ async function getData() {
 
     console.log(data);
 
-    
-    let maxAmount = data.reduce((prev, curr) => {
-        if (curr.amount > prev) {
-            return curr.amount; 
-        } else {
-            return prev
-        }
-    }, 0)
+    let maxAmount = Math.max(...data.map((bar) => bar.amount));
+    // let maxAmount = data.reduce((prev, curr) => {
+    //     if (curr.amount > prev) {
+    //         return curr.amount; 
+    //     } else {
+    //         return prev
+    //     }
+    // }, 0)
     
     data.forEach(bar => {
         let barContainer = document.createElement('div'); 
         let barEl = document.createElement('div');
+        let barTooltipEl = document.createElement('div');
         let labelEl = document.createElement('div');
         let today = new Date().toString().substring(0,3).toLowerCase(); 
         
         barContainer.className = "bar-container"; 
 
         barEl.className = `bar ${bar.day == today ? 'today' : ''}`
-        barEl.style.height = (bar.amount / maxAmount * 100) + '%';
+        barEl.style.maxHeight = (bar.amount / maxAmount * 100) + '%';
         
+        barTooltipEl.innerText = "$" + bar.amount; 
+        barTooltipEl.className = "bar-tooltip"; 
+
         labelEl.innerText = bar.day;
+        labelEl.className = "label"; 
         
+        barEl.appendChild(barTooltipEl); 
+
         barContainer.appendChild(barEl);
         barContainer.appendChild(labelEl);
         graphArea.appendChild(barContainer);
